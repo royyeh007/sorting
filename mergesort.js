ba86779bb956780ad4731942c52579c150fb2176
Roy Yeh <royyeh007@gmail.com>
2018-09-17 15:18:42 -0500

    dfsdf

--- mergesort.js
+++ mergesort.js
@@ -5,19 +5,39 @@ function split(array) {
   else {
     let firstHalf = array.slice(0, Math.floor(array.length / 2));
     let secondHalf = array.slice(Math.floor(array.length / 2))
-    // split(firstHalf);
-    // split(secondHalf);
-    // return (split(firstHalf), split(secondHalf));
-    return merge(split(firstHalf), split(secondHalf));
+    
+   return [firstHalf, secondHalf]
   }
 }
 split([4, 3, 2, 1]);
 
 
 function merge(firstHalf, secondHalf){
-    return [firstHalf, secondHalf];
+  let result = [];
+  let firstIndx = 0;
+  let secondIndx = 0;
+  while(firstIndx < firstHalf.length || secondIndx < secondHalf.length){
+    if(firstHalf[firstIndx] <= secondHalf[secondIndx] || secondHalf.length === secondIndx){
+     result.push(firstHalf[firstIndx])
+     firstIndx ++;
+     }else if (firstHalf[firstIndx] >= secondHalf[secondIndx] || firstHalf.length === firstIndx){
+      result.push(secondHalf[secondIndx])
+      secondIndx ++;
+     }
+    }
+  return result;
 }
+merge([3, 4], [1, 2])
 
-function mergeSort(){
+function mergeSort(array){
+  let newArr = []
+  if(array.length === 1){
+    newArr.push(array);
+  }else{
+    mergeSort(split(array))
+  }
 
+
+
+  return merge(a, b)
 }
